481e1c7ffe5dbf601ea4fae65e22469dea4a72ad
RAZEMANn2 <babykinartem36@gmail.com>
2025-01-19 04:38:59 +0300

    Practice

--- app.py
+++ app.py
@@ -3,21 +3,15 @@ from PIL import Image, ImageFilter
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['UPLOAD_FOLDER'] = 'static/images/uploads'
-db = SQLAlchemy(app)
 
 # Проверка и создание директории для загрузки изображений
 if not os.path.exists(app.config['UPLOAD_FOLDER']):
     os.makedirs(app.config['UPLOAD_FOLDER'])
 
-class ImageModel(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(150), nullable=False)
-
 @app.route('/')
 def home():
-    images = ImageModel.query.all()
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
     return render_template('home.html', images=images)
 
 @app.route('/upload', methods=['POST'])
@@ -30,9 +24,6 @@ def upload():
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
-        new_image = ImageModel(filename=file.filename)
-        db.session.add(new_image)
-        db.session.commit()
         return redirect(url_for('process_image', filename=file.filename))
 
 @app.route('/process_image/<filename>')
@@ -40,8 +31,8 @@ def process_image(filename):
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     with Image.open(filepath) as img:
         img.load()
-        img = img.rotate(90)
         img = img.filter(ImageFilter.BLUR)
+        img = img.rotate(180)
         processed_filename = f"processed_{filename}"
         processed_filepath = os.path.join(app.config['UPLOAD_FOLDER'], processed_filename)
         img.save(processed_filepath)
@@ -56,13 +47,7 @@ def delete_image(filename):
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     if os.path.exists(filepath):
         os.remove(filepath)
-    image = ImageModel.query.filter_by(filename=filename).first()
-    if image:
-        db.session.delete(image)
-        db.session.commit()
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
